22df41e0afe9870cadca527dda4ee865da61576f
Sankarshan-Mudkavi <Sankarshan.Mudkavi@Gmail.com>
2018-09-01 02:10:18 -0400

    Updated website

--- static/js/front.js
+++ static/js/front.js
@@ -107,6 +107,13 @@ function sliders () {
       itemsMobile: [480, 1]
     })
 
+    $('.communities').owlCarousel({
+      items: 4,
+      itemsDesktopSmall: [990, 3],
+      itemsTablet: [768, 2],
+      itemsMobile: [480, 1]
+    })
+
     $('.project').owlCarousel({
       navigation: true, // Show next and prev buttons
       navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
